475ad6f8100f4b39de27807d6d270ba1beeaa066
qiangruowang <529342000@qq.com>
2020-07-07 17:22:35 +0800

    创建羡慕

---
+++ WindowsService1/Help.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService1
+{
+    public class Help
+    {
+        /// <summary>
+        /// 保存至本地文件
+        /// </summary>
+        /// <param name="ETMID"></param>
+        /// <param name="content"></param>
+        public static void SaveRecord(string content)
+        {
+            if (string.IsNullOrEmpty(content))
+            {
+                return;
+            }
+            FileStream fileStream = null;
+            StreamWriter streamWriter = null;
+            try
+            {
+                string path = Path.Combine(System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase, string.Format("{0:yyyyMMdd}", DateTime.Now));
+
+
+                using (fileStream = new FileStream(path, FileMode.Append, FileAccess.Write))
+                {
+                    using (streamWriter = new StreamWriter(fileStream))
+                    {
+                        streamWriter.Write(content);
+
+
+                        if (streamWriter != null)
+                        {
+                            streamWriter.Close();
+                        }
+                    }
+
+                    if (fileStream != null)
+                    {
+                        fileStream.Close();
+                    }
+                }
+            }
+            catch { }
+        }
+    }
+}
---
+++ WindowsService1/Service1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer recordTimer;
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            IntialSaveRecord();
+        }
+
+        protected override void OnStop()
+        {
+            if (recordTimer != null)
+            {
+                recordTimer.Dispose();
+            }
+        }
+
+
+        /// <summary>
+        /// 定时检查，并执行方法
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="e"></param>
+        private void IntialSaveRecord()
+        {
+            TimerCallback timerCallback = new TimerCallback(CallbackTask);
+
+            AutoResetEvent autoEvent = new AutoResetEvent(false);
+            recordTimer = new Timer(timerCallback, autoEvent, 0, 10000);//其中参数10000表示延时执行服务的时间间隔，毫秒为单位
+        }
+        //方法
+        private void CallbackTask(Object stateInfo)
+        {
+            Help.SaveRecord(string.Format(@"当前记录时间：{0},状况：程序运行正常！", DateTime.Now));
+        }
+    }
+}
